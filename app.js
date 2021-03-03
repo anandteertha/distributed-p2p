d306e2ae4d8be08c76dace4f0228e8d715448fec
anandteertha <anandteertha.ramesh18@siesgst.ac.in>
2021-03-03 14:59:11 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var PORT = process.env.PORT || 5000;
 var express = require('express');
+var session = require('express-session')
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var path = require('path');
+var bodyParser = require('body-parser')
 var list = [];
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 io.on('connection',function(socket){
   console.log('a client joined..');
   socket.on('addClient',function(data){
     list.push(data);
+    console.log('the users online are: ',list);
     console.log('number of users online are: ',list.length);
   });
 
@@ -17,6 +27,19 @@ io.on('connection',function(socket){
     //socket.emit('recieveMessage',data);
     console.log('data recieved is: ',data.str);
   });
+
+  socket.on('typing',function(data) {
+    io.sockets.emit('typing',data)
+  })
+
+  socket.on('doneTyping',(data) => {
+    io.sockets.emit('doneTyping',data)
+  })
+
+  socket.on('sendOnline',(data) => {
+    console.log('list: ',list);
+    io.sockets.emit('getOnline',list)
+  })
 
 });
 
@@ -32,10 +55,23 @@ app.get('/html/chatpage.html',function(req,res){
   res.sendFile(path.join(__dirname+'/html'+'/chatpage.html'));
 });
 
+app.get('/chatpage',(req,res) => {
+  res.sendFile(path.join(__dirname+'/html'+'/chatpage.html'))
+})
+
+app.get('/wallpaper',(req,res) => {
+  res.sendFile(path.join(__dirname+'/chatWallpaper.jpg'))
+})
+
 app.post('/chatpage',function(req,res){
+  console.log(req.body);
+  if(req.body.usname.trim().length == 0) {
+    res.redirect('/')
+  }
   res.sendFile(path.join(__dirname+'/html'+'/chatpage.html'));
 });
 
+
 http.listen(PORT,function(){
-  console.log('listening on port 3000..');
+  console.log('listening on port 5000..');
 });
